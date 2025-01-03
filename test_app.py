314ee8e0ad68e4bb2f6ca1dae2d5c3170930d205
zabuza <114882014+zabubu@users.noreply.github.com>
2025-01-03 10:20:44 +0100

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,11 @@
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        with app.test_client() as client:
+            res = client.get('/add?a=3&b=4')
+            self.assertEqual(res.json['result'], 7)
+
+if __name__ == '__main__':
+    unittest.main()
